ae55258f103cb22a4665a66201db4694627a1c04
Dominic Balloch <dominicballoch@dominics-air.home>
2021-05-07 19:10:49 +0100

    removed response body, not needed

--- src/main/java/bookstore/controller/BookRestController.java
+++ src/main/java/bookstore/controller/BookRestController.java
@@ -1,8 +1,8 @@
 package bookstore.controller;
 
-import bookstore.exception.BookNotFoundException;
 import bookstore.domain.Book;
 import bookstore.domain.Category;
+import bookstore.exception.BookNotFoundException;
 import bookstore.service.AdminService;
 import bookstore.service.BookService;
 import lombok.SneakyThrows;
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
@@ -35,7 +34,6 @@ public class BookRestController {
     private AdminService adminService;
 
     @GetMapping(value = "/findAllBooks/rest", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public ResponseEntity<List<Book>> findAllBooks() {
         final List<Book> bookList = bookService.findAllBooks();
         return new ResponseEntity<List<Book>>(bookList, HttpStatus.OK);
@@ -43,7 +41,6 @@ public class BookRestController {
 
     @SneakyThrows
     @GetMapping(value = "/findBook/rest/{isbn}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public ResponseEntity<Optional<Book>> findBookByIsbn(@PathVariable final long isbn) {
         final Optional<Book> book = Optional.ofNullable(bookService.findBookByIsbn(isbn)
                 .orElseThrow(() -> new BookNotFoundException(BOOK_NOT_FOUND)));
@@ -51,7 +48,6 @@ public class BookRestController {
     }
 
     @PostMapping(value = "/addNewBookToBookstore/rest", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public ResponseEntity<Book> addNewBookToBookstore(@PathVariable final long isbn, @PathVariable
                                 final Category category, @PathVariable final String title, @PathVariable
                                         final String author, @PathVariable final BigDecimal price) {
@@ -60,14 +56,12 @@ public class BookRestController {
     }
 
     @DeleteMapping(value = "/deleteBookFromBookstore/rest", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
     public ResponseEntity<Book> deleteBookFromBookstore(@PathVariable final long isbn) {
         adminService.deleteSingleBookFromBookstoreByIsbn(isbn);
         return new ResponseEntity<Book>(HttpStatus.OK);
     }
 
 //    @PatchMapping(value = "/amendBookInBookstore/rest", produces = MediaType.APPLICATION_JSON_VALUE)
-//    @ResponseBody
 //    public ResponseEntity<Book> amendBookInBookstore() {
 //        bookService.amendBookInBookstore();
 //        return new ResponseEntity<Book>(HttpStatus.OK);
