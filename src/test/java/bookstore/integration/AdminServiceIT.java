717f25fb15881ecd4e3658b27aabed4dd1b3835a
Dominic Balloch <bigdom76@gmail.com>
2021-10-25 14:45:32 +0100

    Adding profile to IT

--- src/test/java/bookstore/integration/AdminServiceIT.java
+++ src/test/java/bookstore/integration/AdminServiceIT.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
+@ActiveProfiles("test")
 public class AdminServiceIT {
 
 	private static final String ENDPOINT = "/rest";
