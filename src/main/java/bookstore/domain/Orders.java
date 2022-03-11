f1520b8436e2ba5d093d2ea3495f683e7f7356e4
Dominic Balloch <bigdom76@gmail.com>
2022-03-11 16:18:06 +0000

    adding more entities and updating sql

---
+++ src/main/java/bookstore/domain/Orders.java
@@ -0,0 +1,57 @@
+package bookstore.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder(toBuilder = true)
+@Data
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+@ToString(of = {"orderId", "totalItems", "totalPrice", "orderDate"})
+public class Orders implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long orderId;
+
+    @Column(name = "total_items")
+    private int totalItems;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime orderDate;
+
+    @ManyToOne(cascade = { CascadeType.ALL })
+    @JoinColumn(name="userId")
+    private List<Users> users;
+
+    @OneToMany(mappedBy = "isbn", fetch = FetchType.EAGER, cascade = { CascadeType.ALL })
+    private List<Book> books;
+}
---
+++ src/main/java/bookstore/domain/Users.java
@@ -0,0 +1,54 @@
+package bookstore.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Builder(toBuilder = true)
+@Data
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"userId"})
+@ToString(of = {"userId", "firstName", "lastName", "addressLine1", "addressLine2", "postCode"})
+public class Users implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId")
+    private Long userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "address_line_1")
+    private String addressLine1;
+
+    @Column(name = "address_line_2")
+    private String addressLine2;
+
+    @Column(name = "post_code")
+    private String postCode;
+
+    @OneToMany(mappedBy = "orderId", fetch = FetchType.EAGER, cascade = { CascadeType.ALL })
+    private Orders orders;
+}
