9af092e17a150498b651b51f4f15a2cb9643f6c3
Dominic Balloch <bigdom76@gmail.com>
2022-05-10 22:07:17 +0100

    Updating config

--- src/main/java/bookstore/controller/config/SpringdocConfig.java
+++ src/main/java/bookstore/controller/config/SpringdocConfig.java
@@ -3,18 +3,28 @@ package bookstore.controller.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringdocConfig {
 
+    @Value(value = "${spring.application.name}")
+    private String name;
+
+    @Value(value = "${spring.application.description}")
+    private String description;
+
+    @Value(value = "${spring.build.version})")
+    private String version;
+
     @Bean
     public OpenAPI apiInfo() {
         return new OpenAPI()
-                .info(new Info().title("Bookstore API")
-                        .description("A personal project and sandpit for dev proof of concepts")
-                        .version("0.0.1-SNAPSHOT")
+                .info(new Info().title(name)
+                        .description(description)
+                        .version(version)
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
